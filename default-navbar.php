b6b9a4269fdcfdb26c89efc4b9f04eed117e47bf
Carlos <componentblox@gmail.com>
2019-10-12 02:32:34 -0400

    Update default-navbar.php
    Added Class to Button Togglor

--- default-navbar.php
+++ default-navbar.php
@@ -9,7 +9,7 @@
     	    <i class="fab fa-bootstrap fa-4x"></i>
     	<?php }?>
     	
-    	<button class="navbar-toggler collapsed border-0" type="button" data-toggle="collapse" data-target="#navbarIcon" aria-controls="navbarIcon" aria-expanded="false" aria-label="Toggle navigation">
+    	<button id="toggler" class="navbar-toggler collapsed border-0" type="button" data-toggle="collapse" data-target="#navbarIcon" aria-controls="navbarIcon" aria-expanded="false" aria-label="Toggle navigation">
     		<?php if (file_exists($child_theme_directory . '/template-parts/navbar/navbar-toggler.php') && filesize($child_theme_directory . '/template-parts/navbar/navbar-toggler.php')) { get_template_part('/template-parts/navbar/navbar' , 'toggler');
     		} else {?><span class="navbar-toggler-icon"></span><?php }?>
     	</button>
