4ba4f86ce2c1dcfa7c6e5f34a22ad3e9d39cf0df
Carlos <componentblox@gmail.com>
2019-11-21 16:24:41 -0500

    Updated post-content.php

--- post-content.php
+++ post-content.php
@@ -19,7 +19,7 @@ if (has_post_thumbnail()) { ?>
 	<img class="img-fluid rounded w-100" src="<?php echo esc_html(get_the_post_thumbnail_url());?>" alt="<?php echo esc_html($alt_text);?>"/>
 <?php }?>
 
-<h1 class="mt-4"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title();?></a></h1>
+<h1 class="mt-4"><?php the_title();?></h1>
 
 <div class="post_meta mb-3">
 	<span class="author badge badge-light border p-2"><?php esc_attr_e( 'Published by', 'bootstrap-component-blox' );?> <?php the_author_posts_link();?></span>
