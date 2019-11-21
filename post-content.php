ef93008789d3d7331342b4259cffdc1d8a3e7184
Carlos <componentblox@gmail.com>
2019-11-21 16:06:51 -0500

    Updated post-content.php

--- post-content.php
+++ post-content.php
@@ -13,6 +13,7 @@
 
 $image_id = get_post_thumbnail_id(get_the_ID());
 $alt_text = get_post_meta($image_id , '_wp_attachment_image_alt', true);
+$category = get_the_category();
 
 if (has_post_thumbnail()) { ?>
 	<img class="img-fluid rounded w-100" src="<?php echo esc_html(get_the_post_thumbnail_url());?>" alt="<?php echo esc_html($alt_text);?>"/>
@@ -20,14 +21,17 @@ if (has_post_thumbnail()) { ?>
 
 <h1 class="mt-4"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title();?></a></h1>
 
-<div class="post_meta">
-	<span class="author"><?php esc_attr_e( 'Published by', 'bootstrap-component-blox' );?> <?php the_author_posts_link();?></span>
-	<span class="date">| <?php the_time('F j, Y');?></span>
-	<?php if(the_category()) {?> <span class="category">| <?php the_category(', ');?> </span><?php }?>
+<div class="post_meta mb-3">
+	<span class="author badge badge-light border p-2"><?php esc_attr_e( 'Published by', 'bootstrap-component-blox' );?> <?php the_author_posts_link();?></span>
+	<span class="date badge badge-light border p-2"> <?php the_time('F j, Y');?></span> 
+	<span class="date badge badge-light border p-2">Category: <?php if(!empty($category)) { echo '<a href="' . get_category_link($category[0]->term_id) . '">' . esc_html( $category[0]->name );}?></a></span>
+	
 	<?php if(comments_open()) {?>
-	<span class="comments">
+	
+	<span class="comments badge badge-light border p-2">
 		<?php if (comments_open(get_the_ID())) comments_popup_link( __( 'Leave your thoughts', 'bootstrap-component-blox' ), __( '1 Comment', 'bootstrap-component-blox' ), __( '% Comments', 'bootstrap-component-blox' ));?>
 	</span>
+	
 	<?php }?>
 </div>
 <?php the_content();?>
