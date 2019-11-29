a4d89b5800206f16c174e4c96bb1512e04bd81d0
Carlos <componentblox@gmail.com>
2019-11-29 15:50:05 -0500

    Updated default-blog.php file

--- default-blog.php
+++ default-blog.php
@@ -13,10 +13,10 @@
 	<div class="card d-block shadow p-3 border-0">
 		<img class="post-image w-100 mb-2" src="<?php echo esc_html(get_the_post_thumbnail_url());?>" alt="<?php echo esc_html($alt_text);?>">
 		<h4 class="post-title mb-0"><?php the_title();?></h4>
-		<p class="post-meta"><small>By <?php the_author_posts_link();?> | <?php the_time('F j, Y');?> | <?php if(!empty($category)) { echo '<a href="' .  esc_html(get_category_link($category[0]->term_id)) . '">' . esc_html( $category[0]->name );}?></a> </small></p>
+		<p class="post-meta"><small>By <?php the_author_posts_link();?> | <?php the_time('F j, Y');?> | <?php if(!empty($category)) { echo '<a href="' .  esc_html(get_category_link($category[0]->term_id)) . '">' . esc_html( $category[0]->name ) . '</a>';}?></small></p>
 		<p class="post-content"><?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?></p>
 		<p class="post-link text-right mb-0">
-			<a aria-label="Read More" class="btn btn-sm btn-dark" href="<?php the_permalink();?>" role="button">Read More</i></a>
+			<a aria-label="Read More" class="btn btn-sm btn-dark" href="<?php the_permalink();?>" role="button">Read More</a>
 		</p>
 	</div>
 </div>
