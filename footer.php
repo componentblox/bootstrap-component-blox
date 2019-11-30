c711a632b68232a3210cdd409cc1ea04d9bd5012
Carlos <componentblox@gmail.com>
2019-11-30 13:28:21 -0500

    Updated footer.php

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@ $child_theme_directory = get_stylesheet_directory();
 get_template_part('footer-widgets');?>
 	
 	<footer class="text-center py-3 <?php echo esc_html($footer_classes);?>" role="contentinfo">
-		<div class="site-info"><?php if($footer_credit){echo ($footer_credit);} else {?>&copy;<?php echo esc_html(date('Y'));?> Copyright <a href="https://theme.componentblox.com"><?php bloginfo('name'); ?></a>. <?php esc_html('Powered by ', 'bootstrap-component-blox'); ?><a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//getbootstrap.com/" title="Bootstrap"> Bootstrap</a>.<?php }?></div>
+		<div class="site-info"><?php if($footer_credit){echo ($footer_credit);} else {?>&copy;<?php echo esc_html(date('Y'));?> Copyright <a href="https://componentblox.com"><?php bloginfo('name'); ?></a>. <?php esc_html('Powered by ', 'bootstrap-component-blox'); ?><a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//getbootstrap.com/" title="Bootstrap"> Bootstrap</a>.<?php }?></div>
 	</footer>
     <?php wp_footer(); ?>
 
