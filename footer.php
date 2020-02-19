95799cfdbdbab7d33d4f4a93a39fd565f7c4613e
Carlos <cmontalvo@uandidezign.com>
2020-02-19 01:13:17 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -10,13 +10,14 @@
 $footer_credit = get_theme_mod('footer_credit');
 $footer_classes = get_theme_mod('footer_classes');
 $child_theme_directory = get_stylesheet_directory();
+$arr = array('href' => array(), 'br' => array(), 'strong' => array());
 
 cb_before_footer();
 
 get_template_part('footer-widgets');?>
 	
 	<footer class="text-center p-3 <?php echo esc_html($footer_classes);?>" role="contentinfo">
-		<div class="site-info"><?php if($footer_credit){ echo wp_kses($footer_credit , 'bootstrap-component-blox');} else {?>&copy;<?php echo esc_html(date('Y'));?> <?php esc_html_e('Copyright' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://componentblox.com/', 'bootstrap-component-blox' ) ); ?>"><?php esc_html_e('Component Blox.' , 'bootstrap-component-blox');?></a> <?php esc_html_e('Powered by' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap-component-blox' ) ); ?>">
+		<div class="site-info"><?php if($footer_credit){ echo wp_kses($footer_credit, $arr, 'bootstrap-component-blox');} else {?>&copy;<?php echo esc_html(date('Y'));?> <?php esc_html_e('Copyright' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://componentblox.com/', 'bootstrap-component-blox' ) ); ?>"><?php esc_html_e('Component Blox.' , 'bootstrap-component-blox');?></a> <?php esc_html_e('Powered by' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap-component-blox' ) ); ?>">
 			<?php 
 			printf( esc_html__( 'WordPress', 'bootstrap-component-blox' )); ?></a> &amp; <a href="<?php echo esc_url( __( 'https://getbootstrap.com/', 'bootstrap-component-blox' ) ); ?>" title="Bootstrap"> <?php esc_html_e('Bootstrap' , 'bootstrap-component-blox');?></a>.<?php }?></div>
 	</footer>
