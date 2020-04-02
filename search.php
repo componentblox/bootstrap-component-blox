7d988023c33dbafab4c352618f921d34d5c7b4dd
Carlos <cmontalvo@uandidezign.com>
2020-04-02 00:09:19 -0400

    Updated Search Template

--- search.php
+++ search.php
@@ -15,10 +15,10 @@
 <main id="main-container" role="main" class="container-fluid px-0">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <div class="entry-content">
-            <div class="container">
+            <div class="container mt-4">
                 <div class="row">
                     <div class="col-12 text-center">
-                        <h1 class="page-title mb-5"><?php printf( esc_html__( 'Search', 'bootstrap-component-blox' ), '<span>' . get_search_query() . '</span>' );?></h1>
+                        <h1 class="page-title mb-5"><?php printf( esc_html__( 'Search Results', 'bootstrap-component-blox' ), '<span>' . get_search_query() . '</span>' );?></h1>
                     </div>
                     <?php 
                         get_template_part('loop');
