bfce0201497c994062b6b7b3043b02648a6b8c54
Carlos <cmontalvo@uandidezign.com>
2020-04-02 00:30:05 -0400

    Updated Looped Template

--- loop.php
+++ loop.php
@@ -18,22 +18,21 @@ if (have_posts()): while (have_posts()) : the_post();
 		<div class="card p-3">	
 			<?php if (has_post_thumbnail()) { ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-				<img class="w-100 mb-2" src="<?php echo esc_url($image);?>" alt="<?php echo $alt;?>">
+				<img class="w-100 mb-3" src="<?php echo esc_url($image);?>" alt="<?php echo $alt;?>">
 			</a>
 			<?php }?>
 			
-			<h4><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
+			<h4 class="mb-0"><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
 			<hr>
 	    	<p><?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?></p>
-
 			<p class="mb-0"><a href="<?php the_permalink();?>" class="btn btn-dark text-white"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
 			<?php wp_link_pages();?></p>
 			</div>	
 		</div>	
 
 	<?php 
-	endwhile; 
-	else: 
+		endwhile; 
+		else: 
 	?>
 	
 	<article>
