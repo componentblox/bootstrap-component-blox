c2e2377bb07dba61c6995b37f28ad0aea003a6d9
Carlos <cmontalvo@uandidezign.com>
2020-04-02 00:06:53 -0400

    Update Search and Loop templates

--- loop.php
+++ loop.php
@@ -9,14 +9,15 @@
 
 if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<div class="mb-3">		
+		<div class="col-12 col-lg-4 mb-4 mb-lg-0">	
+		<div class="card p-3">	
 			<?php if (has_post_thumbnail()) : ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 				<img class="img-fluid rounded" style="width: 100%;" src="<?php echo esc_url(get_the_post_thumbnail_url());?>" alt="">
 			</a>
 			<?php endif; ?>
 			
-			<h1 class="mt-4"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title_attribute();?></a></h1>
+			<h4><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title_attribute();?></a></h4>
 
 			<div class="post_meta">
 				<span class="author"><?php esc_html_e( 'Published by', 'bootstrap-component-blox' );?> <?php the_author_posts_link();?></span>
@@ -29,10 +30,11 @@ if (have_posts()): while (have_posts()) : the_post(); ?>
 			
 			<hr>
 
-	    	<?php the_content();?>
+	    	<p><?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?></p>
 
-			<a href="<?php the_permalink();?>" class="btn btn-dark text-white"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
-			<?php wp_link_pages(); ?> 	
+			<p class="mb-0"><a href="<?php the_permalink();?>" class="btn btn-dark text-white"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
+			<?php wp_link_pages();?></p>
+			</div>	
 		</div>	
 
 	<?php 
--- search.php
+++ search.php
@@ -11,24 +11,24 @@
 
 <?php get_header();?>
 
-<div class="container my-5">
-    <div class="row">
-        <div class="col-lg-8">
-            
-			<h1 class="page-title mb-5"><?php printf( esc_html__( 'Search', 'bootstrap-component-blox' ), '<span>' . get_search_query() . '</span>' );?></h1>
 
-			<?php 
-                get_template_part('loop');
-                get_template_part('pagination'); 
-            ?>
-        
+<main id="main-container" role="main" class="container-fluid px-0">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="entry-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1 class="page-title mb-5"><?php printf( esc_html__( 'Search', 'bootstrap-component-blox' ), '<span>' . get_search_query() . '</span>' );?></h1>
+                    </div>
+                    <?php 
+                        get_template_part('loop');
+                        get_template_part('pagination'); 
+                    ?>
+                </div>
+            </div>
         </div>
         
-        <div class="col-md-4">
-            <?php get_sidebar();?>
-        </div>
-        
-    </div>
-</div>
+    </article>
+</main>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
