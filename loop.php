edc788954cb86036d9d6771fcab2d1f8b96dad54
Carlos <cmontalvo@uandidezign.com>
2020-04-13 12:54:47 -0400

    Fixed loop template

--- loop.php
+++ loop.php
@@ -7,27 +7,33 @@
  * @package bootstrap-component-blox
  */
 
-if (have_posts()): while (have_posts()) : the_post(); 
-	
+
+?>
+<div class="row">
+
+<?php
+	if (have_posts()): while (have_posts()) : the_post(); 
 	// vars
 	$thumbnail_id = get_post_thumbnail_id( $post->ID );
 	$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-	$image = wp_get_attachment_image_url( $thumbnail_id, 'medium' );?>
+	$image = wp_get_attachment_image_url( $thumbnail_id, 'medium' );
+?>
 
-	<div class="card mx-auto p-3 mb-4">	
-		<?php if (has_post_thumbnail()) { ?>
-		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-			<img class="w-100 mb-3" src="<?php echo esc_url($image);?>" alt="<?php echo $alt;?>">
-		</a>
-		<?php }?>
-		
-		<h4 class="mb-0"><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
-		<hr>
-    	<?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?>
-		<p class="mb-0"><a href="<?php the_permalink();?>" class="btn btn-dark text-white"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
-		<?php wp_link_pages();?></p>
-	</div>	
-
+	<div class="col-12 col-lg-4">
+		<div class="card d-block p-3 mb-4">	
+			<?php if (has_post_thumbnail()) { ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+				<img class="w-100 mb-3" src="<?php echo esc_url($image);?>" alt="<?php echo $alt;?>">
+			</a>
+			<?php }?>
+			
+			<h4 class="mb-0"><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
+			<hr>
+	    	<?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?>
+			<p class="mb-0"><a href="<?php the_permalink();?>" class="btn btn-dark text-white my-2"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
+			<?php wp_link_pages();?>
+		</div>
+	</div>
 
 	<?php 
 		endwhile; 
@@ -37,5 +43,6 @@ if (have_posts()): while (have_posts()) : the_post();
 	<div class="col-12 text-center my-4">
 		<h2><?php esc_html_e( 'Sorry, nothing to display.', 'bootstrap-component-blox' ); ?></h2>
 	</div>
-
+</div>	
+	
 <?php endif; ?>
