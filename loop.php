e1bfd403fa34794749178227220807d937e66e57
Carlos <cmontalvo@uandidezign.com>
2020-04-13 08:38:09 -0400

    Fixed Loop Template

--- loop.php
+++ loop.php
@@ -14,21 +14,20 @@ if (have_posts()): while (have_posts()) : the_post();
 	$alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 	$image = wp_get_attachment_image_url( $thumbnail_id, 'medium' );?>
 
-	<div class="col-12 col-lg-4 mb-4">	
-		<div class="card p-3">	
-			<?php if (has_post_thumbnail()) { ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-				<img class="w-100 mb-3" src="<?php echo esc_url($image);?>" alt="<?php echo $alt;?>">
-			</a>
-			<?php }?>
-			
-			<h4 class="mb-0"><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
-			<hr>
-	    	<p><?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?></p>
-			<p class="mb-0"><a href="<?php the_permalink();?>" class="btn btn-dark text-white"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
-			<?php wp_link_pages();?></p>
-			</div>	
-		</div>	
+	<div class="card p-3">	
+		<?php if (has_post_thumbnail()) { ?>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+			<img class="w-100 mb-3" src="<?php echo esc_url($image);?>" alt="<?php echo $alt;?>">
+		</a>
+		<?php }?>
+		
+		<h4 class="mb-0"><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
+		<hr>
+    	<p><?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?></p>
+		<p class="mb-0"><a href="<?php the_permalink();?>" class="btn btn-dark text-white"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
+		<?php wp_link_pages();?></p>
+	</div>	
+
 
 	<?php 
 		endwhile; 
