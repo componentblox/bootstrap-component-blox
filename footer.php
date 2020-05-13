f2fb4aa95851d36b301a1e3d344f82933cb56f1b
Carlos <cmontalvo@uandidezign.com>
2020-05-13 00:54:39 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -7,17 +7,6 @@
  * @package bootstrap-component-blox
  */
 
-$arr = array(
-	'a' => array(
-		'href' => array(),
-		'title' => array(),
-		'target' => array(),
-	),
-	'br' => array(),
-	'em' => array(),
-	'strong' => array(),
-);
-
 bcb_before_footer();
 
 get_template_part('footer-widgets');
@@ -26,7 +15,7 @@ if(get_theme_mod('navbar_search')) {
 }?>
 	
 	<footer class="text-center p-3 <?php echo esc_html(get_theme_mod('footer_classes'));?>" role="contentinfo">
-		<div class="site-info"><?php if(get_theme_mod('footer_credit')){ echo wp_kses(get_theme_mod('footer_credit'), $arr, 'bootstrap-component-blox');} else {?>&copy;<?php echo esc_html(date('Y'));?> <?php esc_html_e('Copyright' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://componentblox.com/', 'bootstrap-component-blox' ) ); ?>"><?php esc_html_e('Component Blox.' , 'bootstrap-component-blox');?></a> <?php esc_html_e('Powered by' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap-component-blox' ) ); ?>">
+		<div class="site-info"><?php if(get_theme_mod('footer_credit')){ echo get_theme_mod('footer_credit');} else {?>&copy;<?php echo esc_html(date('Y'));?> <?php esc_html_e('Copyright' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://componentblox.com/', 'bootstrap-component-blox' ) ); ?>"><?php esc_html_e('Component Blox.' , 'bootstrap-component-blox');?></a> <?php esc_html_e('Powered by' , 'bootstrap-component-blox');?> <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap-component-blox' ) ); ?>">
 			<?php 
 			printf( esc_html__( 'WordPress', 'bootstrap-component-blox' )); ?></a> &amp; <a href="<?php echo esc_url( __( 'https://getbootstrap.com/', 'bootstrap-component-blox' ) ); ?>" title="Bootstrap"> <?php esc_html_e('Bootstrap' , 'bootstrap-component-blox');?></a>.<?php }?>
 		</div>
