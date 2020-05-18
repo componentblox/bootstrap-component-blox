82f3bb1773abc56c91028c02ab32855cd79630f5
Carlos <cmontalvo@uandidezign.com>
2020-05-17 21:54:56 -0400

    Center text in loop.

--- template-parts/post/post-loop.php
+++ template-parts/post/post-loop.php
@@ -9,36 +9,39 @@
 
 
 ?>
-<div class="row px-3">
+<div class="row px-3 justify-content-center">
 	<div class="card-deck">
 
 <?php
 	if (have_posts()) {
 	while (have_posts()) { the_post(); 
 		
-	$image_id = get_post_thumbnail_id( $post->ID );
+	$image_id = get_post_thumbnail_id($post->ID);
 ?>
 
-	<div class="card d-block p-3 mb-4" style="min-width: 18rem;">
-		<?php if (has_post_thumbnail()) { ?>
-		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-			<img class="w-100 mb-3" src="<?php bcb_image_url($image_id, 'medium');?>" alt="<?php bcb_image_alt($image_id);?>""/>
-		</a>
-		<?php }?>
-		
-		<h4 class="mb-0"><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
-		<hr>
-    	<?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?>
-		<p class="mb-0"><a href="<?php the_permalink();?>" class="btn btn-dark text-white my-2"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a></p>
-		<?php wp_link_pages();?>
-	</div>
+		<div class="card d-block p-3 mb-4" style="min-width: 18rem;">
+			<?php if (has_post_thumbnail()) { ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
+				<img class="w-100 mb-3" src="<?php bcb_image_url($image_id, 'medium');?>" alt="<?php bcb_image_alt($image_id);?>"/>
+			</a>
+			<?php }?>
+			
+			<h4 class="mb-0">
+				<a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a>
+			</h4>
+			<hr>
+	    	<?php echo esc_html(wp_trim_words( get_the_content(),20, '...' ));?>
+			<p class="mb-0">
+				<a href="<?php the_permalink();?>" class="btn btn-dark text-white my-2"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
+			</p>
+			<?php wp_link_pages();?>
+		</div>
 
-	<?php }} else { ?>
-	
-	<div class="col-12 my-4 text-center">
-		<h2><?php esc_html_e( 'Sorry, nothing to display.', 'bootstrap-component-blox' ); ?></h2>
-	</div>
-	
-    <?php }?>
+		<?php }} else { ?>
+			<div class="card border-0">
+				<h2><?php esc_html_e('Sorry, nothing to display.' , 'bootstrap-component-blox');?></h2>
+			</div>
+		
+	    <?php }?>
     </div>	
 </div>	
