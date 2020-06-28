63b37c9a511c193c5dc8f8bb8fbe26b0b72f9ae5
Carlos <cmontalvo@uandidezign.com>
2020-06-28 02:03:59 -0400

    Fixed loop template columns

--- template-parts/post/post-loop.php
+++ template-parts/post/post-loop.php
@@ -9,18 +9,17 @@
 
 ?>
 
-<div class="row justify-content-center">
-	<div class="card-deck flex-fill">
+<div class="row row-cols-1 row-cols-lg-3">
 
 	<?php
 		if (have_posts()) {
 		while (have_posts()) { the_post(); 		
-		$image_id = get_post_thumbnail_id($post->ID);
 	?>
-		<div class="card p-3 mb-4" style="min-width: 18rem;">
+	<div class="col mb-4">
+		<div class="card p-3 mb-4 h-100">
 			<?php if (has_post_thumbnail()) { ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
-				<img class="w-100 mb-3" src="<?php bcb_image_url($image_id, 'medium');?>" alt="<?php bcb_image_alt($image_id);?>"/>
+				<img class="w-100 mb-3" src="<?php bcb_image_url(bcb_image_id(), 'medium');?>" alt="<?php bcb_image_alt(bcb_image_id());?>"/>
 			</a>
 			<?php }?>
 			<h4><a href="<?php the_permalink();?>" title="<?php the_title_attribute();?>"><?php the_title_attribute();?></a></h4>
@@ -29,13 +28,13 @@
 				<a href="<?php the_permalink();?>" class="btn btn-dark"><?php esc_html_e('Read More' , 'bootstrap-component-blox');?></a>
 			</div>
 		</div>
+	</div>
 
-	<?php }} else { ?>
+	<?php } } else { ?>
 		<div class="col-12 col-lg-6 mx-auto mb-4">
-			<p class="alert alert-warning text-center"><?php esc_html_e('We could not find your search. Please try Again.' , 'bootstrap-component-blox');?></p>
+			<p class="alert alert-warning text-center"><?php esc_html_e('No search result found. Please try again.' , 'bootstrap-component-blox');?></p>
 			<?php get_template_part('searchform');?>
 		</div>
-		
 	<?php }?>
-    </div>	
+ 
 </div>	
