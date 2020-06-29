2fa2cf1e829d25824b5a2cb6672975bfbb0efcee
Carlos <cmontalvo@uandidezign.com>
2020-06-29 12:34:42 -0400

    Fixed function to get icons when duplicated for child theme

--- functions.php
+++ functions.php
@@ -434,7 +434,7 @@ add_action('wp_footer', 'bcb_include_search_modal');
  */
 function bcb_icon($name, $size = '20')  {?>
     <svg width="<?php echo $size;?>" height="<?php echo $size;?>" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <?php include(__DIR__ . '/icons/' . $name . '.svg');?>
+        <?php include(locate_template('/icons/' . $name . '.svg'));?>
     </svg>
 <?php }
 
