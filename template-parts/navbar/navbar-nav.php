f3bca92543a3547f18e6bc41cddea119d7c7af98
Carlos <cmontalvo@uandidezign.com>
2020-08-24 13:52:55 -0400

    Fixed logo redirect back to homepage

--- template-parts/navbar/navbar-nav.php
+++ template-parts/navbar/navbar-nav.php
@@ -12,7 +12,7 @@
 <nav class="navbar-light navbar navbar-expand-lg py-0 <?php echo esc_attr(get_theme_mod('navbar_classes'));?>">
 	<div class="container-fluid <?php echo esc_attr(get_theme_mod('navbar_inner_classes'));?>">
 						
-    	<a id="navbar-brand-logo" class="navbar-brand" href="/"> 
+    	<a id="navbar-brand-logo" class="navbar-brand" href="<?php echo get_site_url();?>"> 
             <?php if(has_custom_logo()) {?>
                 <img src="<?php echo esc_url(bcb_image_url(get_theme_mod('custom_logo')));?>" alt="<?php echo esc_attr(get_bloginfo());?>" />	
             <?php } else {
