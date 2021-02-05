a4e13ac887941d6b6eb160b5e13ea6c2e017d2e2
Carlos <cmontalvo@uandidezign.com>
2021-02-05 00:34:46 -0500

    Update 1.6.1
    Added Woocommerce support
    Fixed post content post-meta

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ add_action( 'after_setup_theme', 'bcb_content_width', 0 );
  * Theme implement editor styling. 
  */
 function bcb_add_editor_styles() {
-	add_editor_style( get_stylesheet_uri() );
+    add_editor_style( get_stylesheet_uri() );
 }
 add_action('init', 'bcb_add_editor_styles');
 
@@ -76,7 +76,7 @@ if(!is_admin()) {
         wp_enqueue_style('bootstrap-component-blox', get_stylesheet_uri());
         wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.bundle.min.js' , array() , '5.0.0' , true);
         wp_enqueue_script('bootstrap-component-blox', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0' , true);
-        wp_enqueue_script('bootstrap-component-blox-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1.0', true );	
+        wp_enqueue_script('bootstrap-component-blox-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1.0', true );  
     }
     add_action('wp_enqueue_scripts', 'bcb_enqueue_theme_styles_scripts');
 }
@@ -375,6 +375,14 @@ function bcb_mime_types($mimes) {
     return $mimes;
 }
 add_filter('upload_mimes', 'bcb_mime_types');
+
+/**
+ * Hook: Woocommerce Support
+ */
+function bcb_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'bcb_add_woocommerce_support' );
 
 /**
  * Hook: Before Footer.
--- template-parts/post/post-content.php
+++ template-parts/post/post-content.php
@@ -13,24 +13,25 @@
 
 $category = get_the_category();
 
-if (has_post_thumbnail()) { ?>
+if (has_post_thumbnail()) {?>
 	<img id="bcb-featured-image" class="w-100" src="<?php echo bcb_image_url(bcb_image_id());?>" alt="<?php echo bcb_image_alt(bcb_image_id());?>"/>
 <?php }?>
 
 <h1 class="mt-4"><?php the_title_attribute();?></h1>
+<div class="post-meta mb-3">
+	<span class="author me-2 small"><?php esc_html_e( 'Published by ', 'bootstrap-component-blox' );?> <?php the_author_posts_link();?></span>
+	<span class="date me-2 small"><?php the_time('F j, Y');?></span> 
+	
+	<?php if(!empty($category)) {?>
+	    <span class="category small"><?php esc_html_e('Category: ' , 'bootstrap-component-blox'); if(!empty($category)) { echo '<a href="' .  esc_url(get_category_link($category[0]->term_id)) . '">' . esc_html( $category[0]->name );}?></a></span>
+    <?php }?>
 
-<div class="post_meta mb-3">
-	<span class="author badge badge-light border p-2"><?php esc_html_e( 'Published by ', 'bootstrap-component-blox' );?> <?php the_author_posts_link();?></span>
-	<span class="date badge badge-light border p-2"> <?php the_time('F j, Y');?></span> 
-	<span class="category badge badge-light border p-2"><?php esc_html_e('Category: ' , 'bootstrap-component-blox'); if(!empty($category)) { echo '<a href="' .  esc_url(get_category_link($category[0]->term_id)) . '">' . esc_html( $category[0]->name );}?></a></span>
-	
 	<?php if(comments_open()) {?>
-	
-	<span class="comments badge badge-light border p-2">
-		<?php if (comments_open(get_the_ID())) comments_popup_link( __( 'Leave your thoughts', 'bootstrap-component-blox' ), __( '1 Comment', 'bootstrap-component-blox' ), __( '% Comments', 'bootstrap-component-blox' ));?>
-	</span>
-	
+    	<span class="comments badge badge-light border p-2">
+    		<?php if (comments_open(get_the_ID())) comments_popup_link( __( 'Leave your thoughts', 'bootstrap-component-blox' ), __( '1 Comment', 'bootstrap-component-blox' ), __( '% Comments', 'bootstrap-component-blox' ));?>
+    	</span>
 	<?php }?>
 </div>
+
 <?php the_content();?>
 <?php if(comments_open()) { get_template_part('template-parts/post/post' , 'comments'); }?>
