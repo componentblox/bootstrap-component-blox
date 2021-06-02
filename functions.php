0773feb122451ef131669e0bff5f700958e227d9
Carlos <cmontalvo@uandidezign.com>
2021-06-02 15:10:26 -0400

    Updates
    Updated to Font Awesome v5.15.3
    Updated Bootstrap v5.0.1

--- functions.php
+++ functions.php
@@ -71,10 +71,10 @@ require_once( get_template_directory() . '/custom-comments.php' );
 // Load styles and scripts.
 if(!is_admin()) {
     function bcb_enqueue_theme_styles_scripts(){
-        wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', '5.0.0' , false);
-        wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.15.2/css/all.css', false, '5.15.2');
+        wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', '5.0.1' , false);
+        wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.15.3/css/all.css', false, '5.15.2');
         wp_enqueue_style('bootstrap-component-blox', get_stylesheet_uri());
-        wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.bundle.min.js' , array() , '5.0.0' , true);
+        wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.bundle.min.js' , array() , '5.0.1' , true);
         wp_enqueue_script('bootstrap-component-blox', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0' , true);
         wp_enqueue_script('bootstrap-component-blox-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1.0', true );  
     }
